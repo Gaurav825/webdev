720adc8e54ffb9b0e6a89a908ea9e006e6ef0a19
GAURAV <64942636+Gaurav825@users.noreply.github.com>
2023-08-05 19:44:52 +0530

    Create spreadoperator.js

---
+++ spreadoperator.js
@@ -0,0 +1,11 @@
+const string1 = 'Hello';
+[...string1].forEach((x)=>console.log(x))
+
+const string2="gaurav"
+string1.split('').forEach((x) => console.log(x)); // Outputs each character of the string
+
+// for of loop used to iterate over object
+for (variable of string2) {
+  console.log(variable);
+  
+}
